a49230bc8dc3d7034864981c36d536fc10faf20a
CrazyLenny <ykxlenny@gmail.com>
2017-04-27 17:41:38 -0400

    First commit

---
+++ src/flappyBird/FlappyBird.java
@@ -0,0 +1,38 @@
+package flappyBird;
+
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+
+public class FlappyBird {
+	
+	public static FlappyBird flappyBird;
+	
+	public final int WIDTH = 800, HEIGHT = 800;
+	
+	public Renderer renderer;
+	
+	public FlappyBird() {
+		JFrame jframe = new JFrame();
+		
+		renderer = new Renderer();
+		
+		jframe.add(renderer);
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.setSize(WIDTH,HEIGHT);
+		jframe.setResizable(false);
+		jframe.setVisible(true);
+	}
+	
+	public void repaint(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		flappyBird = new FlappyBird();
+		
+	}
+
+
+}
